a4bf512b74b10f7839cea110e78f489b3ce6aa11
wadepvenga <134316590+wadepvenga@users.noreply.github.com>
2025-07-25 11:07:58 -0300

    Fix: Corrige base path para domínio customizado e move CNAME para public/

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.VERCEL ? '/' : '/a-kanban-brasil/',
+  base: '/',
 }));
